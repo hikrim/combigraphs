592aeaff401bb4e6dde746445b26d821f4d212b8
Artem Petlin <1132246846@pfur.ru>
2025-12-25 01:42:21 +0300

    Add change

--- src/combigraphs/combfuncs/comb.py
+++ src/combigraphs/combfuncs/comb.py
@@ -1,11 +1,11 @@
-from combigraphs.combfuncs import fact
+from combigraphs.combfuncs.fact import factorial
 
 def combination(n, k):
     if (k == 0) or (n == k):
         return 1
     if (k == 1) or (k == n - 1):
         return n
-    return (fact.factorial(n) // (fact.factorial(k) * fact.factorial(n - k)))
+    return (factorial(n) // (factorial(k) * factorial(n - k)))
 
 def test_combination():
     assert combination(4, 2) == 6
