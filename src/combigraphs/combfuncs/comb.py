6cec5b740bbdf66b0ca755ba45ef0066578dc60c
Artem Petlin <1132246846@pfur.ru>
2025-12-24 23:54:36 +0300

    Upload combigraphs

---
+++ src/combigraphs/combfuncs/comb.py
@@ -0,0 +1,16 @@
+from . import fact
+
+def combination(n, k):
+    if (k == 0) or (n == k):
+        return 1
+    if (k == 1) or (k == n - 1):
+        return n
+    return (fact.factorial(n) // (fact.factorial(k) * fact.factorial(n - k)))
+
+def test_combination():
+    assert combination(4, 2) == 6
+    print("test_combination complete!")
+    
+if __name__ == '__main__':
+    test_combination()
+    print(combination(10, 3))
